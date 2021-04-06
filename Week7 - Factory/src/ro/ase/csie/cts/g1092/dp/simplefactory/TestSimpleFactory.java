fde22f9a2ae9602f7967fdba1471128ab116298a
Rafael1558 <belcurafael17@stud.ase.ro>
2021-04-06 10:35:52 +0300

    Before the break

---
+++ Week7 - Factory/src/ro/ase/csie/cts/g1092/dp/simplefactory/TestSimpleFactory.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1092.dp.simplefactory;
+
+public class TestSimpleFactory {
+
+	public static void main(String[] args) {
+		
+		SuperHero superman = new SuperHero("SuperMan");
+		// superman.setWeapon(new Bazooka("Boom"));
+		// superman.setWeapon(new MachineGun("MG", 500, 250));
+		
+		
+		AbstractWeapon bazooka = WeaponsFactory.getWeapon(WeaponType.BAZOOKA, "BOOM");
+		superman.setWeapon(bazooka);
+		superman.setWeapon(WeaponsFactory.getWeapon(WeaponType.MACHINE_GUN, "MG"));
+	}
+}
